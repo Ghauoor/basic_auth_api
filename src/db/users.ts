56a9c0467395885e60faa83799af0f4d740fa64f
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2023-12-30 12:09:15 +0500

    REST AUTH API

---
+++ src/db/users.ts
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true },
+  authentication: {
+    password: { type: String, required: true, select: false },
+    salt: { type: String, select: false },
+    sessionToken: { type: String, select: false },
+  },
+});
+
+export const UserModel = mongoose.model("User", userSchema);
+
+// Actions
+export const getUser = async () => {
+  const user = await UserModel.find().exec();
+  return user;
+};
+
+export const getUserByEmail = (email: string) => UserModel.findOne({ email });
+
+export const getUserSessionToken = (sessionToken: string) =>
+  UserModel.findOne({
+    "authentication.sessionToken": sessionToken,
+  });
+
+export const getUserById = (id: string) => UserModel.findById(id);
+
+export const createUser = (values: Record<string, any>) =>
+  new UserModel(values).save().then((user) => user.toObject());
+
+export const deleteUserById = (id: string) =>
+  UserModel.findByIdAndDelete({ _id: id });
+
+export const updateUserById = (id: string, values: Record<string, any>) =>
+  UserModel.findByIdAndUpdate(id, values);
